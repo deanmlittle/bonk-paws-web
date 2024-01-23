49fa63e789d664abca7f0a418d53a6186e9dbb5a
ShrinathNR <nr.shrinath@gmail.com>
2024-01-23 12:27:44 +0530

    css ok

--- src/app/components/OrganizationList.tsx
+++ src/app/components/OrganizationList.tsx
@@ -31,9 +31,9 @@ const OrganizationList: React.FC<{}> = () => {
       <div>
         <Modal organization={modalOrganization}  isOpen={isOpen} setIsOpen={setIsOpen} />
         <div className='flex flex-wrap justify-center'>
-          <input className='text-black flex-auto mx-20 p-3 rounded focus:border-yellow-300' type="text" id = "search"  placeholder="Search for Charities..."
+          <input className='text-black mx-20 sm:mt-10 lg:mt-0 p-3 rounded w-1/2' type="text" id = "search"  placeholder="Search for Charities..."
             onChange={(e)=>setSearch(e.target.value)}/>
-          <div id="charities" className="grid grid-cols-1 mt-20 md:grid-cols-2 lg:grid-cols-4 gap-6 md:px-20">
+          <div id="charities" className=" grid grid-cols-1 mt-20 md:grid-cols-2 lg:grid-cols-4 gap-6 md:px-20">
             {
               organizations.filter((organizations: ShortOrganization)=> {
                 return search.toLowerCase() === '' ? organizations : organizations.name.toLowerCase().includes(search);
