d719b2b7f8a3cb3cdafc622aa3fddd8619278cdb
godzidcrypto <godzidcrypto@gmail.com>
2024-04-12 16:25:41 +0800

    style: connect wallet button

--- src/app/components/WalletButton.tsx
+++ src/app/components/WalletButton.tsx
@@ -1,11 +1,14 @@
-import React from "react"
+import React from "react";
 
 const WalletButton: React.FC<{ onClick: () => void }> = ({ onClick }) => {
-    return (
-        <button className="bg-red-500 hover:bg-red-400 text-white font-semibold py-3 px-5 border border-red-600 hover:border-red-600 rounded-lg" onClick={onClick}>
-            Connect Wallet
-        </button>
-    )
-}
+  return (
+    <button
+      className="bg-bonk-orange hover:opacity-50 text-white font-semibold py-3 px-5 border shadow-md duration-300 transition-all cursor-pointer rounded-lg"
+      onClick={onClick}
+    >
+      Connect Wallet
+    </button>
+  );
+};
 
-export default WalletButton
+export default WalletButton;
